67a485ddc65fcede379ce8bba1061910367f1bd0
Mahamadoudmb <62967057+Mahamadoudmb@users.noreply.github.com>
2020-04-07 19:55:49 +0200

    Add files via upload

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail a THP" #cette ligne nous dit que l'on va compter le nombre d'heure de travail a thp en nous affichant le text comme tel
+puts "Travail : #{10 * 5 * 11}" # cette ligne  sert a afficher "Travail" + une instruction en commentaire qui represente 10 heure 5 jours et 11 semaines convertit en heures
+puts "En minutes ca fait : #{10 * 5 * 11 * 60}" # cette ligne nous indique qu'elle va convertir les heure de travaille en minute à trravers un text et nous donner le resultat a travers une operation
+
+puts "Et en secondes ?" # cette ligne nous indique une question qui est de savoir la conversion du resultat precedents en secondes
+
+puts 10 * 5 * 11 * 60 * 60 # cette ligne nous indique directement le resultat de la question en effectuant une operation
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # cette ligne nous indique de 2 operation qui sont mis dans un ordre croissant tout en posant une question 
+
+puts 3 + 2 < 5 - 7 # cette ligne  nous indique que c'est faux en effectuant l'operation liee à la question precedente 
+
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" # cette ligne nous indique la valeur de la 1ere operation dans l'operation preccedentes sous forme de question reponse
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" # cette ligne  nous indique la valeur  de la 2eme operation sous forme de question reponse
+
+puts "Ok, c'est faux alors !" # cette ligne nous indique que l'operation  etait vraisemblabement fausse
+
+puts "C'est drole ca, faisons-en plus :" # cette ligne nous indique que l'on va effectuer d'autre operation similaires
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # cette ligne nous indique  2 operation qui sont mis dans un ordre decroissant sous fome de question tout en donnant la reponse
+puts "Est-ce que 5 est superieur ou egal a -2 ? #{5 >= -2}" # cette ligne nous indique  2 operation qui sont mis dans un ordre decroissant ou egal sous fome de question tout en donnant la reponse
+puts "Est-ce que 5 est inferieur ou egal a -2 ? #{5 <= -2}"  # cette ligne nous indique  2 operation qui sont mis dans un ordre croissant ou egal sous fome de question tout en donnant la reponse
---
+++ exo_06.rb
@@ -0,0 +1,7 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{10 * 5 * 11}"
+puts "Et en minutes ca fait : #{60 * 10 * 5 * 11}"
+
